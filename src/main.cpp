56cd44db648d3e3e07e09b4347f4950a95b03137
li-zhenjiong <544363139@qq.com>
2023-09-19 00:02:50 +0800

    finish RongYao 3rd question

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,17 @@
 #include <iostream>
-#include <set>
+#include <unordered_set>
 #include <vector>
-
-struct Point {
-  int x;
-  int y;
-  Point(int _x, int _y) : x(_x), y(_y) {};
-};
-
 
 class Solution {
  public:
   std::vector<std::vector<int> > grid;
-  std::vector<int> dead_monsters;
+  std::unordered_set<int> dead_monsters;
   int n;
 
   Solution(std::vector<std::vector<int> > &input, int _n)
     : n(_n), grid(input), dead_monsters() {
-    if (!dfs(0,0))
-      std::cout << 0 << std::endl;
-    else {
-      std::set<int> d(dead_monsters.cbegin(), dead_monsters.cend());
-      std::cout << d.size() << std::endl;
-    }
+    dfs(0,0);
+    std::cout << dead_monsters.size() << std::endl;
   }
 
   bool dfs(int x, int y);
@@ -32,10 +21,10 @@ class Solution {
 int main() {
   int n = 5;
   std::vector<std::vector<int> > grid(n);
-  grid[0] = std::vector<int>({0,0,0,1,0});
+  grid[0] = std::vector<int>({0,0,0,1,1});
   grid[1] = std::vector<int>({0,0,-1,-1,0});
-  grid[2] = std::vector<int>({0,-1,0,0,0});
-  grid[3] = std::vector<int>({0,0,1,0,-1});
+  grid[2] = std::vector<int>({0,-1,0,1,0});
+  grid[3] = std::vector<int>({0,1,1,0,-1});
   grid[4] = std::vector<int>({0,0,1,1,0});
   
   Solution(grid, n);
@@ -47,9 +36,11 @@ bool Solution::dfs(int x, int y) {
   if (x < 0 || x > n-1 || y < 0 || y > n-1 || this->grid[x][y] == -1)
     return false;
   if (x == n-1 && y == n-1) return true;
-  if (dfs(x+1,y) || dfs(x,y+1)) {
+  bool next_row = dfs(x+1,y);
+  bool next_col = dfs(x,y+1);
+  if (next_row || next_col) {
     if (this->grid[x][y] == 1)
-      this->dead_monsters.push_back(x*n+y);
+      this->dead_monsters.insert(x*n+y);
     return true;
   }
   else {
