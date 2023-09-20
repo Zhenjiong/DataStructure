2ddc2407d747ca22904d6287d30a4a030b49adcd
li-zhenjiong <544363139@qq.com>
2023-09-20 17:32:33 +0800

    TODO bit calcu

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,11 @@
 #include <iostream>
-#include <unordered_set>
-#include <vector>
-
-class Solution {
- public:
-  std::vector<std::vector<int> > grid;
-  std::unordered_set<int> dead_monsters;
-  int n;
-
-  Solution(std::vector<std::vector<int> > &input, int _n)
-    : n(_n), grid(input), dead_monsters() {
-    dfs(0,0);
-    std::cout << dead_monsters.size() << std::endl;
-  }
-
-  bool dfs(int x, int y);
-
-};
+#include <string>
+#include <list>
 
 int main() {
-  int n = 5;
-  std::vector<std::vector<int> > grid(n);
-  grid[0] = std::vector<int>({0,0,0,1,1});
-  grid[1] = std::vector<int>({0,0,-1,-1,0});
-  grid[2] = std::vector<int>({0,-1,0,1,0});
-  grid[3] = std::vector<int>({0,1,1,0,-1});
-  grid[4] = std::vector<int>({0,0,1,1,0});
+  std::string s = "Hello";
+  s.erase(s.begin(), s.begin()+1);
   
-  Solution(grid, n);
-  std::cout << "Hello World!" << std::endl;
+  std::cout << s;
   return 0;
-}
-
-bool Solution::dfs(int x, int y) {
-  if (x < 0 || x > n-1 || y < 0 || y > n-1 || this->grid[x][y] == -1)
-    return false;
-  if (x == n-1 && y == n-1) return true;
-  bool next_row = dfs(x+1,y);
-  bool next_col = dfs(x,y+1);
-  if (next_row || next_col) {
-    if (this->grid[x][y] == 1)
-      this->dead_monsters.insert(x*n+y);
-    return true;
-  }
-  else {
-    return false;
-  }
 }
