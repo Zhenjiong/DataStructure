577b94b7f669f16700540ab57bb78978ada75dc6
li-zhenjiong <544363139@qq.com>
2023-10-10 10:07:12 +0800

    constructor in OOP

--- records/Question.md
+++ records/Question.md
@@ -634,3 +634,73 @@ bool Solution::bfs(int x, int y, std::vector<int> &path) {
   }
 }
 ```
+
+### 继承中调用构造和析构的顺序
+
+```c++
+// 作者：程序员bingo
+// 链接：https://zhuanlan.zhihu.com/p/104014640
+// 来源：知乎
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+#include <iostream>
+
+using namespace std;
+
+class CBase {
+public:
+  CBase() {
+    Init();
+  }
+
+  virtual ~CBase() {
+    Uninit();
+  };
+
+  virtual void Init() {
+    Fun();
+  }
+
+  virtual void Uninit() {
+    cout << "CBase::Uninit" << endl;
+  }
+
+  virtual void Fun() {
+    cout << "CBase::Fun" << endl;
+  }
+
+  int base_value = 0x1234;
+};
+
+class CDerived : public CBase {
+public:
+  CDerived() {
+    Init();
+  }
+
+  virtual ~CDerived() {
+    Uninit();
+  }
+
+  virtual void Init() {
+    cout << "CDerived::Init" << endl;
+  }
+
+  virtual void Uninit() {
+    cout << "CDerived::Uninit" << endl;
+  }
+
+  virtual void Fun() {
+    cout << "CDerived::Fun" << endl;
+  }
+
+  int derived_value = 0x5678;
+};
+
+int main() {
+  CDerived* derived = new CDerived();
+  delete derived;
+  derived = nullptr;
+  return 0;
+}
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,60 @@
 #include <iostream>
 
-struct C {
-  int c;
-  int c2;
-};
-struct A {
+using namespace std;
 
-  virtual void print() {
-    std::cout << "A" << std::endl;
+class CBase {
+public:
+  CBase() {
+    Init();
   }
 
-  virtual void get() {
-    std::cout << a << std::endl;
+  virtual ~CBase() {
+    Uninit();
+  };
+
+  virtual void Init() {
+    Fun();
   }
 
-  int a;
-  int a3;
+  virtual void Uninit() {
+    cout << "CBase::Uninit" << endl;
+  }
+
+  virtual void Fun() {
+    cout << "CBase::Fun" << endl;
+  }
+
+  int base_value = 0x1234;
 };
 
-struct B : A {
-  int b;
+class CDerived : public CBase {
+public:
+  CDerived() {
+    Init();
+  }
+
+  virtual ~CDerived() {
+    Uninit();
+  }
+
+  virtual void Init() {
+    cout << "CDerived::Init" << endl;
+  }
+
+  virtual void Uninit() {
+    cout << "CDerived::Uninit" << endl;
+  }
+
+  virtual void Fun() {
+    cout << "CDerived::Fun" << endl;
+  }
+
+  int derived_value = 0x5678;
 };
 
-int main () {
-  B bb;
-  bb.b = 10;
-  A *pA = &bb;
-  B *pB = dynamic_cast<B*>(pA);
-  std::cout << pB->b << std::endl;
-  std::cout << sizeof(B) << std::endl;
-  C sc{1};
-  std::cout << sc.c2 << " " << sc.c << std::endl;
+int main() {
+  CDerived* derived = new CDerived();
+  delete derived;
+  derived = nullptr;
   return 0;
 }
