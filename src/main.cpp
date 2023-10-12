5bcdfce428181199e7c8d7ce7aa3fdadb76f29ff
li-zhenjiong <544363139@qq.com>
2023-10-12 21:18:29 +0800

    TODO rvalue and lvalue

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,10 @@
 #include <iostream>
-#include <memory>
-#include <vector>
 
-using namespace std;
-
-struct A {
-  int a;
-
-  A(const A &) = delete;
-
-	A(A &&) = default;
-
-	A &operator=(const A &other) noexcept = delete;
-
-	A &operator=(A &&other) noexcept = delete;
-};
+char quan[] = "jkl;";
 
 int main () {
-  
-  vector<A> data;
-  data.push_back(A{10});
-  data.push_back(A{30});
-
-  auto data2(std::move(data));
-
-  vector<unique_ptr<int> > x;
-  x.push_back(make_unique<int>(10));
-  x.push_back(make_unique<int>(50));
-
-  auto y(std::move(x));
-
+  char s[] = "asdf";
+  int i = 4;
+  std::cout << &"kkkk" << " " << &quan << " " << &s << " " << &i << std::endl;
   return 0;
 }
