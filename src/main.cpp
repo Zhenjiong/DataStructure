ffbdce98e6ad651a55cfd2de6b6b015837eb1630
li-zhenjiong <544363139@qq.com>
2023-10-18 11:40:48 +0800

    lvalue rvalue test

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,15 @@ void Test(T &&t) {
   PrintV(std::move(t));
 }
 
+struct A {
+  int b;
+  int &a;
+
+  A (int _a, int _b)
+    : b(_b), a(_a) {}
+
+};
+
 int main() {
   Test(1); // lvalue rvalue rvalue
   int a = 1;
@@ -26,5 +35,14 @@ int main() {
   Test(std::forward<int>(a)); // lvalue rvalue rvalue
   Test(std::forward<int&>(a)); // lvalue lvalue rvalue
   Test(std::forward<int&&>(a)); // lvalue rvalue rvalue
+
+  int x = 20;
+  int y = x & (-x);
+  std::cout << y << std::endl;
+
+  A(21, 12);
+  int k = 10;
+  int &&pp = 20;
+  std::cout << std::is_rvalue_reference_v<decltype(pp)> << std::endl;
   return 0;
 }
