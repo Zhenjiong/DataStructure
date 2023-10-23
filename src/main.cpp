ac85c738d19ce0174585d6e505d0c70b8e6c3eda
li-zhenjiong <544363139@qq.com>
2023-10-23 21:10:35 +0800

    shared_ptr, leak memory

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,18 @@
 #include <iostream>
 #include <string>
-
-char itoc(int x) {
-  if (x == 0)
-    return '0';
-  if (x == 1)
-    return '1';
-  if (x == 2)
-    return '2';
-  if (x == 3)
-    return '3';
-  if (x == 4)
-    return '4';
-  if (x == 5)
-    return '5';
-  if (x == 6)
-    return '6';
-  if (x == 7)
-    return '7';
-  if (x == 8)
-    return '8';
-  if (x == 9)
-    return '9';
-}
+#include <memory>
+using namespace std;
 
 int main() {
-  std::string s1, s2;
-  // std::cin >> s1;
-  // std::cin >> s2;
-  s1 = "234"; s2 = "123";
+  shared_ptr<int> sp = make_shared<int>(100);
+  weak_ptr<int> wp(sp);
 
-  auto it = s1.rbegin();
-  auto it2 = s2.rbegin();
-  std::string result = s1;
-  // int res = 0;
-  int pos = 0;
-  bool bring = false;
-  while (it != s1.rend() && it2 != s2.rend()) {
-    int m = *it - *it2;
-    if (bring) {
-      --m;
-      bring = false;
-    }
-    if (m >= 0) {
-      result[pos] = itoc(m);
-      // res = res + m * pos;
-      ++pos;
-    }
-    else {
-      if (it+1 != s1.rend()) {
-        result[pos] = itoc(10+m);
-        // res = res + (10+m) * pos;
-        ++pos;
-        bring = true;
-      }
-      // minus result
-      else {
-        result = '-' + result;
-      }
-    }
-    ++it2;
-    ++it;
+  if (!wp.expired()) {
+    auto temp = wp.lock();
+    cout << *temp << endl;
+    cout << sp.use_count() << endl;
   }
-
-  int k = 0;
+  cout << sp.use_count() << endl;
   
-  while (result[k] == '0') {
-    ++k;
-  }
-  auto res = result.substr(k, result.size()-k);
-  std::cout << res;
   return 0;
 }
