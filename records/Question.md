cbb1c86a5e43d2a8d0597514e8329ba8379579fc
li-zhenjiong <544363139@qq.com>
2023-11-09 23:51:42 +0800

    quickSort

--- records/Question.md
+++ records/Question.md
@@ -354,6 +354,62 @@ TODO
 
 ## Practice
 
+### 快速排序
+
+分治的思想，随机选取一个基准值，大于基准值的放右边，小于基准值的放左边
+
+再分别对左右两边重复同样的操作，直到左右两侧是对一个数排序时终止
+
+```c++
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void quickSort(vector<int>& nums, int l, int r) {
+  // break condition: only one element to sort
+  if (l >= r) return;
+
+  int i = l;
+  int j = r;
+  // choose a random num as a reference axis
+  // here I use the last num
+  int index = nums[r];
+  while (i < j) {
+    // find the bigger one then swap to right
+    while (i < j && nums[i] <= index)
+      ++i;
+    if (i < j) {
+      swap(nums[i], nums[j]);
+      --j;
+    }
+
+    // find the smaller or equal one then swap to left
+    while (i < j && nums[j] > index)
+      --j;
+    if (i < j) {
+      swap(nums[i], nums[j]);
+      ++i;
+    }
+  }
+  // now i == j and nums[i] is the correct place
+  // for this index
+  nums[i] = index;
+
+  quickSort(nums, l, i-1);
+  quickSort(nums, i+1, r);
+}
+
+int main () {
+  vector<int> in = {5,2,1,4,3,1};
+  int n = in.size();
+  quickSort(in, 0, n-1);
+  for (auto i : in)
+    cout << i << " ";
+  return 0;
+}
+```
+
 ### 归并排序
 
 148 leetcode 排序链表
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,47 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#include <queue>
-
 
 using namespace std;
 
+void quickSort(vector<int>& nums, int l, int r) {
+  // break condition: only one element to sort
+  if (l >= r) return;
+
+  int i = l;
+  int j = r;
+  // choose a random num as a reference axis
+  // here I use the last num
+  int index = nums[r];
+  while (i < j) {
+    // find the bigger one then swap to right
+    while (i < j && nums[i] <= index)
+      ++i;
+    if (i < j) {
+      swap(nums[i], nums[j]);
+      --j;
+    }
+
+    // find the smaller or equal one then swap to left
+    while (i < j && nums[j] > index)
+      --j;
+    if (i < j) {
+      swap(nums[i], nums[j]);
+      ++i;
+    }
+  }
+  // now i == j and nums[i] is the correct place
+  // for this index
+  nums[i] = index;
+
+  quickSort(nums, l, i-1);
+  quickSort(nums, i+1, r);
+}
+
 int main () {
-  // int n;
-  // cin >> n;
-  // string s;
-  // cin >> s;
-
-  int n = 2;
-  string s = "1:0,0:1";
-
-  vector<vector<int> > path(n);
-  vector<int> inDegree(n, 0);
-
-  
-
+  vector<int> in = {5,2,1,4,3,1};
+  int n = in.size();
+  quickSort(in, 0, n-1);
+  for (auto i : in)
+    cout << i << " ";
   return 0;
 }
