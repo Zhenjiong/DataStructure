2c1f15e5f27c179ba1407491f2143f3d092b5c23
li-zhenjiong <544363139@qq.com>
2023-11-14 08:27:38 +0800

    count islands

--- src/main.cpp
+++ src/main.cpp
@@ -3,55 +3,43 @@
 
 using namespace std;
 
-void quickSort(vector<int>& nums, int l, int r) {
-  // break condition: only one element to sort
-  if (l >= r) return;
+class Solution {
+ public:
+  
+  vector<vector<int> > grid;
+  int m;
+  int n;
 
-  int i = l;
-  int j = r;
-  // choose a random num as a reference axis
-  // here I use the last num
-  int index = nums[r];
-  while (i < j) {
-    // find the bigger one then swap to right
-    while (i < j && nums[i] <= index)
-      ++i;
-    if (i < j) {
-      swap(nums[i], nums[j]);
-      --j;
-    }
-
-    // find the smaller or equal one then swap to left
-    while (i < j && nums[j] > index)
-      --j;
-    if (i < j) {
-      swap(nums[i], nums[j]);
-      ++i;
-    }
+  Solution(vector<vector<int> > _grid, int _m, int _n)
+    : grid(_grid), m(_m), n(_n) {
+    
   }
-  // now i == j and nums[i] is the correct place
-  // for this index
-  nums[i] = index;
 
-  quickSort(nums, l, i-1);
-  quickSort(nums, i+1, r);
-}
-
-void bubbleSort(vector<int> &nums) {
-  for (int i = 0; i < nums.size() - 1; ++i) {
-    for (int j = 0; j < nums.size() - i - 1; ++j) {
-      if (nums[j] > nums[j+1])
-        std::swap(nums[j], nums[j+1]);
-    }
-  }
-}
+  bool bfs(int x, int y);
+  bool dfs(int x, int y);
+};
 
 int main () {
-  vector<int> in = {5,2,1,4,3,1};
-  int n = in.size();
-  quickSort(in, 0, n-1);
-  // bubbleSort(in);
-  for (auto i : in)
-    cout << i << " ";
+  vector<vector<int> > grid1 = {{1,0,0},
+                                {0,1,1},
+                                {0,1,1}};
+  
+
+
   return 0;
+}
+
+bool Solution::bfs(int x, int y) {
+  if (x < 0 || x >= m || y < 0 || y >= n)
+    return false;
+  
+  return true;
+  
+}
+
+bool Solution::dfs(int x, int y) {
+  if (x < 0 || x >= m || y < 0 || y >= n)
+    return false;
+  
+  return true;
 }
