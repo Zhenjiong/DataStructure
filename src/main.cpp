784f243c23a191db46a4ab101e4edfd02fd90a37
li-zhenjiong <544363139@qq.com>
2023-11-21 22:04:37 +0800

    need more state

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,36 @@
-#include <cstring>
-
 #include <iostream>
-#include <string>
-#include <unordered_set>
-#include <set>
-#include <functional>  // basic hash template, such as hash<int>
+#include <vector>
+#include <map>
 
-class String {
- public:
+enum State {TOP, BOTTOM, PREEXTEND};
 
-  String(const char*);  
-
-  void test();
- private:
-  
-  char* mData;
+struct Horizonal {
+  int y;
+  int start;
+  int stop;
+  State state;
 };
 
-#define TT int[5]
+struct Point {
+  int x;
+  int y;
+};
+
+class Solution {
+ public:
+  
+  Solution(std::vector<std::vector<int> >& inputs)
+    : data(), hlines(), res() {
+
+    
+  }
+
+  std::vector<Point> data;
+  std::multimap<int, Horizonal> hlines;
+  std::vector<std::vector<Point> > res;
+};
 
 int main() {
-
-  std::string s = "H!";
-  String ms(s.c_str());
-  const char* kk = s.c_str();
-  kk = kk + 2;
-  int* x = new TT;
-  *x = 1;
-  *(x+1) = 2;
-  int *pp = x+1;
-  delete x;
-  x = nullptr;
-  // std::cout << *(x+1);
-}
-
-String::String(const char* in) {
-  if (in == nullptr) {
-    mData = new char [1];
-    mData[0] = '\0';
-  }
-  else {
-    auto size = std::strlen(in);
-    mData = new char [std::strlen(in)+1];
-    std::strcpy(mData, in);
-    std::cout << *mData << std::endl;
-  }
-}
-
-void String::test() {
-
+  
+  return 0;
 }
