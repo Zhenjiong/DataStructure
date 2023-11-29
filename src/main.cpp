3200c1fd0ef0d64a1663959289ddb02c540c2ee9
li-zhenjiong <544363139@qq.com>
2023-11-29 11:43:40 +0800

    maxHeap ready

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+union U1 {
+  int x;
+  float y;
+  char *ch;
+};
+
+union U2 {
+  int x;
+  float y;
+};
+
 int main() {
+
+  std::cout << sizeof(U1) << std::endl;
+  std::cout << sizeof(U2) << std::endl;
   
   return 0;
 }
